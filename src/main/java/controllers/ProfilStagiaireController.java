28e21b26067b0f3bb9941564ac2c1832e15fb574
aureliasegarra <av.segarra@gmail.com>
2022-05-13 15:22:26 +0200

    Layout Navbar

---
+++ src/main/java/controllers/ProfilStagiaireController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+
+import javafx.fxml.FXML;
+
+public class ProfilStagiaireController {
+
+    @FXML private TextField nom;
+    @FXML private TextField prenom;
+    @FXML private TextField email;
+    @FXML private TextField filiere;
+    @FXML private TextField annee;
+    @FXML private TextField linkedin;
+
+}
